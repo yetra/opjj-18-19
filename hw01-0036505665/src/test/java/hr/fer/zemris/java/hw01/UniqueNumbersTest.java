42b1d38527a3df258db402592989e2bf873eaf83
Bruna <brdujmovic@gmail.com>
2019-03-12 22:35:33 +0100

    Add Task 3 test

---
+++ hw01-0036505665/src/test/java/hr/fer/zemris/java/hw01/UniqueNumbersTest.java
@@ -0,0 +1,42 @@
+package hr.fer.zemris.java.hw01;
+
+import static hr.fer.zemris.java.hw01.UniqueNumbers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import hr.fer.zemris.java.hw01.UniqueNumbers.TreeNode;
+
+public class UniqueNumbersTest {
+
+    @Test
+    public void testTreeSize() {
+        TreeNode head = null;
+        assertEquals(0, treeSize(head));
+
+        for (int i = 0; i < 5; i++) {
+            head = addNode(head, i);
+        }
+        assertEquals(5, treeSize(head));
+    }
+
+    @Test
+    public void testContainsValue() {
+        TreeNode head = null;
+        assertFalse(containsValue(head,-1));
+        assertFalse(containsValue(head,4));
+
+        head = addNode(head, -1);
+        assertTrue(containsValue(head, -1));
+        assertFalse(containsValue(head,4));
+    }
+
+    @Test
+    public void testAddNode() {
+        TreeNode head = null;
+        for (int i = -5; i < 5; i++) {
+            assertFalse(containsValue(head, i));
+            head = addNode(head, i);
+            assertTrue(containsValue(head, i));
+        }
+    }
+}
