407158d234f81caaad9de42f78fa828fb6ef389b
Bruna <brdujmovic@gmail.com>
2019-03-17 10:29:51 +0100

    Implement a generic Processor class

---
+++ hw02-0036505665/src/main/java/hr/fer/zemris/java/custom/collections/Processor.java
@@ -0,0 +1,20 @@
+package hr.fer.zemris.java.custom.collections;
+
+/**
+ * This is a generic class that models an object capable of performing some operation on a passed object.
+ * Each specific processor implementation should inherit this class and override its {@code process} method.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class Processor {
+
+    /**
+     * Processes (performs a specific action on) a given object.
+     *
+     * @param value value of the object to process
+     */
+    public void process(Object value) {
+
+    }
+}
