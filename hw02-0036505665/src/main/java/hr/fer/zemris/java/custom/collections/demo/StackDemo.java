05b77d5502472f118fecc0c0821b1d9c8ce03068
Bruna <brdujmovic@gmail.com>
2019-03-17 15:00:12 +0100

    Apply ObjectStack to posix expression evaluation

---
+++ hw02-0036505665/src/main/java/hr/fer/zemris/java/custom/collections/demo/StackDemo.java
@@ -0,0 +1,71 @@
+package hr.fer.zemris.java.custom.collections.demo;
+
+import hr.fer.zemris.java.custom.collections.ObjectStack;
+
+public class StackDemo {
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("The program accepts 1 argument, " + args.length + " were given.");
+            System.exit(1);
+        }
+        String[] expression = args[0].split("\\s+");
+
+        ObjectStack stack = new ObjectStack();
+
+        for (String element : expression) {
+
+            if (element.matches("^[0-9]+$")) {
+                stack.push(Integer.parseInt(element));
+
+            } else {
+                int firstOperand = (int) stack.pop();
+                int secondOperand = (int) stack.pop();
+
+                try {
+                    int result = performOperation(element, firstOperand, secondOperand);
+                    stack.push(result);
+                } catch (ArithmeticException ex) {
+                    System.out.println(ex.getMessage());
+                    System.exit(1);
+                } catch (IllegalArgumentException ex) {
+                    System.out.println(ex.getMessage() + " This posix expression is invalid.");
+                    System.exit(1);
+                }
+            }
+        }
+
+        if (stack.size() != 1) {
+            System.out.println("ERROR Failed to evaluate expression \"" + args[0] + "\".");
+            System.exit(1);
+        } else {
+            System.out.println("Expression evaluates to " + stack.pop() + ".");
+        }
+    }
+
+    private static int performOperation(String operator, int firstOperand, int secondOperand) {
+        switch (operator) {
+            case "+":
+                return firstOperand + secondOperand;
+
+            case "-":
+                return firstOperand - secondOperand;
+
+            case "*":
+                return firstOperand * secondOperand;
+
+            case "/":
+                if (secondOperand == 0) {
+                    throw new ArithmeticException("Cannot divide by zero.");
+                }
+                return firstOperand / secondOperand;
+
+            case "%":
+                return firstOperand % secondOperand;
+
+            default:
+                throw new IllegalArgumentException("Illegal operator " + operator + " was given.");
+        }
+    }
+}
