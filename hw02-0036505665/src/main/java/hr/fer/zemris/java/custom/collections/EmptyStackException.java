700aef0d21a3c5acc8eab42a4165512eb9036db7
Bruna <brdujmovic@gmail.com>
2019-03-21 01:14:26 +0100

    Add javadoc for EmptyStackException

--- hw02-0036505665/src/main/java/hr/fer/zemris/java/custom/collections/EmptyStackException.java
+++ hw02-0036505665/src/main/java/hr/fer/zemris/java/custom/collections/EmptyStackException.java
@@ -1,11 +1,25 @@
 package hr.fer.zemris.java.custom.collections;
 
+/**
+ * An exception for an empty stack.
+ *
+ * @author Bruna Dujmović
+ *
+ */
 public class EmptyStackException extends RuntimeException {
 
+    /**
+     * Default constructor for the {@code EmptyStackException} class.
+     */
     public EmptyStackException() {
         super();
     }
 
+    /**
+     * Constructor with a message about the exception.
+     *
+     * @param message a message about the exception
+     */
     public EmptyStackException(String message) {
         super(message);
     }
