27718cb1eb2a553727c4105d5b439c9bd7a6c0ee
Bruna <brdujmovic@gmail.com>
2019-03-24 19:07:34 +0100

    Implement Node base class

---
+++ hw03-0036505665/src/main/java/hr/fer/zemris/java/custom/scripting/nodes/Node.java
@@ -0,0 +1,49 @@
+package hr.fer.zemris.java.custom.scripting.nodes;
+
+import hr.fer.zemris.java.custom.ArrayIndexedCollection;
+
+/**
+ * The base class for all graph nodes.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class Node {
+
+    /**
+     * A collection of all child nodes of the current node.
+     */
+    private ArrayIndexedCollection nodes;
+
+    /**
+     * Adds a given child node to an internally managed collection of children.
+     *
+     * @param child the child node to add
+     */
+    public void addChildNode(Node child) {
+        if (nodes == null) {
+            nodes = new ArrayIndexedCollection();
+        }
+
+        nodes.add(child);
+    }
+
+    /**
+     * Returns the number of (direct) children on this node.
+     *
+     * @return the number of (direct) children on this node
+     */
+    public int numberOfChildred() {
+        return nodes.size();
+    }
+
+    /**
+     * Returns the child node that is stored on the specified index.
+     *
+     * @return the child node that is stored on the specified index
+     * @throws IndexOutOfBoundsException if the index is not in the correct range
+     */
+    public Node getChild(int index) {
+        return (Node) nodes.get(index);
+    }
+}
