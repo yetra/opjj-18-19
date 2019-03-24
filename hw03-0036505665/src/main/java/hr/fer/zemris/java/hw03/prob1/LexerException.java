08ff2cb7d555847718abc8d576a639c0e2e6d11a
Bruna <brdujmovic@gmail.com>
2019-03-24 10:04:24 +0100

    Add LexerException class

---
+++ hw03-0036505665/src/main/java/hr/fer/zemris/java/hw03/prob1/LexerException.java
@@ -0,0 +1,26 @@
+package hr.fer.zemris.java.hw03.prob1;
+
+/**
+ * An exeption to be thrown if there is an issue with retrieving the next token.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class LexerException extends RuntimeException {
+
+    /**
+     * Default constructor for the {@code LexerException} class.
+     */
+    public LexerException() {
+        super();
+    }
+
+    /**
+     * Constructor with a message about the exception.
+     *
+     * @param message a message about the exception
+     */
+    public LexerException(String message) {
+        super(message);
+    }
+}
