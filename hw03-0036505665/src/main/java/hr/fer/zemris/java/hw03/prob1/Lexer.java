59bf698080edc31b9ec017abfcf5cea987a2c591
Bruna <brdujmovic@gmail.com>
2019-03-24 12:01:59 +0100

    Implement nextToken for inputs without escape characters

--- hw03-0036505665/src/main/java/hr/fer/zemris/java/hw03/prob1/Lexer.java
+++ hw03-0036505665/src/main/java/hr/fer/zemris/java/hw03/prob1/Lexer.java
@@ -46,16 +46,59 @@ public class Lexer {
      */
     public Token nextToken() {
         if (token != null && token.getType() == TokenType.EOF) {
-            throw new LexerException();
+            throw new LexerException("There are no more characters to tokenize.");
         }
 
         skipBlanks();
 
         if (currentIndex >= data.length) {
-            token = new Token(TokenType.EOF, null);
+            return token = new Token(TokenType.EOF, null);
         }
 
+        // TODO make a helper method that returns a complete token String, not just the end index?
+        int tokenEnd;
+        if (Character.isLetter(data[currentIndex])) {
+            tokenEnd = getTokenEnd(currentIndex, Character::isLetter);
+            String word = new String(data, currentIndex, tokenEnd-currentIndex+1);
+
+            token = new Token(TokenType.WORD, word);
+
+        } else if (Character.isDigit(data[currentIndex])) {
+            tokenEnd = getTokenEnd(currentIndex, Character::isDigit);
+            String number = new String(data, currentIndex, tokenEnd-currentIndex+1);
+
+            try {
+                token = new Token(TokenType.NUMBER, Long.parseLong(number));
+            } catch (NumberFormatException ex) {
+                throw new LexerException("Can't parse " + number + " to long.");
+            }
+
+        } else {
+            tokenEnd = currentIndex;
+            char symbol = data[currentIndex];
+
+            token = new Token(TokenType.SYMBOL, symbol);
+        }
+
+        currentIndex = tokenEnd + 1;
         return token;
+    }
+
+    /**
+     * Returns the index of the last character that belongs to the current token.
+     *
+     * @param startIndex the index of
+     * @param tester a tester that checks if the currently observed character still belongs to the current token
+     * @return the index of the last character that belongs to the current token
+     */
+    private int getTokenEnd(int startIndex, CharacterTester tester) {
+        int endIndex = startIndex;
+
+        while (endIndex < data.length && tester.test(data[endIndex])) {
+            endIndex++;
+        }
+
+        return endIndex-1;
     }
 
     /**
@@ -76,5 +119,20 @@ public class Lexer {
      */
     public Token getToken() {
         return token;
+    }
+
+    /**
+     * A functional interface that models a tester of character types.
+     */
+    @FunctionalInterface
+    private static interface CharacterTester {
+
+        /**
+         * Returns {@code true} if the given character satisfies this tester's condition.
+         *
+         * @param ch the character to test
+         * @return {@code true} if the given character satisfies this tester's condition
+         */
+        boolean test(char ch);
     }
 }
