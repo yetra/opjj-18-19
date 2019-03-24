fb3705fb53bd0324fa93aec1ab5b693be63dd046
Bruna <brdujmovic@gmail.com>
2019-03-24 11:10:39 +0100

    Add support for blank/null inputs, reading after EOF

--- hw03-0036505665/src/main/java/hr/fer/zemris/java/hw03/prob1/Lexer.java
+++ hw03-0036505665/src/main/java/hr/fer/zemris/java/hw03/prob1/Lexer.java
@@ -1,5 +1,7 @@
 package hr.fer.zemris.java.hw03.prob1;
 
+import java.util.Objects;
+
 /**
  * This class models a lexer that can tokenize a given input text.
  *
@@ -27,21 +29,43 @@ public class Lexer {
      * Constructs a lexer which tokenizes the given text.
      *
      * @param text the text to tokenize
+     * @throws NullPointerException if the given text is {@code null}
      */
     public Lexer(String text) {
+        Objects.requireNonNull(text);
+
         data = text.toCharArray();
         currentIndex = 0;
-        token = nextToken();
     }
 
     /**
      * Generates and returns the next toxen.
      *
      * @return the next token
-     * @throws LexerException if there is an issue with generating the next token
+     * @throws LexerException if there are no more characters to tokenize
      */
     public Token nextToken() {
-        return null;
+        if (token != null && token.getType() == TokenType.EOF) {
+            throw new LexerException();
+        }
+
+        skipBlanks();
+
+        if (currentIndex >= data.length) {
+            token = new Token(TokenType.EOF, null);
+        }
+
+        return token;
+    }
+
+    /**
+     * A helper method which skips all the blanks in the {@code data} character array.
+     */
+    private void skipBlanks() {
+        while (currentIndex < data.length &&
+                Character.isWhitespace(data[currentIndex])) {
+            currentIndex++;
+        }
     }
 
     /**
@@ -51,6 +75,6 @@ public class Lexer {
      * @return the last generated token
      */
     public Token getToken() {
-        return null;
+        return token;
     }
 }
