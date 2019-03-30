4c8a6365b10214ce363c3467bb39252d2a47abea
Bruna <brdujmovic@gmail.com>
2019-03-30 11:06:31 +0100

    Expand lexer token types

--- hw03-0036505665/src/main/java/hr/fer/zemris/java/custom/scripting/lexer/SmartScriptTokenType.java
+++ hw03-0036505665/src/main/java/hr/fer/zemris/java/custom/scripting/lexer/SmartScriptTokenType.java
@@ -1,10 +1,59 @@
 package hr.fer.zemris.java.custom.scripting.lexer;
 
+/**
+ * An enumeration of {@link SmartScriptLexer} token types.
+ */
 public enum SmartScriptTokenType {
+
+    /**
+     * End of the input.
+     */
     EOF,
+
+    /**
+     * A valid variable name starts with a letter after which follows
+     * zero or more letters, digits or underscores.
+     */
+    VARIABLE_NAME,
+
+    /**
+     * A valid function name starts with "@" after which follows a letter
+     * and after than can follow zero or more letters, digits or underscores.
+     */
+    FUNCTION_NAME,
+
+    /**
+     * A valid tag name is "=", or a valid variable name.
+     */
+    TAG_NAME,
+
+    /**
+     * A symbol such as "*", "-", "@", "$" etc.
+     */
+    SYMBOL,
+
+    /**
+     * A string which can contain escapings.
+     */
     STRING,
+
+    /**
+     * An integer or negative integer (represented in -digit or digit form).
+     */
     INTEGER,
+
+    /**
+     * A decimal number (represented in digit-dot-digit form).
+     */
     DECIMAL,
+
+    /**
+     * The start of the tag: {$.
+     */
     TAG_START,
+
+    /**
+     * The end of the tag: $}.
+     */
     TAG_END
 }
