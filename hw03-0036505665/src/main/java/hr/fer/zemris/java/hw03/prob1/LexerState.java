0942e4caf0a062fa74fb3d54b66b4f59ee1f8deb
Bruna <brdujmovic@gmail.com>
2019-04-04 19:57:23 +0200

    Add missing javadoc for Task 2

--- hw03-0036505665/src/main/java/hr/fer/zemris/java/hw03/prob1/LexerState.java
+++ hw03-0036505665/src/main/java/hr/fer/zemris/java/hw03/prob1/LexerState.java
@@ -7,6 +7,17 @@ package hr.fer.zemris.java.hw03.prob1;
  *
  */
 public enum  LexerState {
+    /**
+     * This state tokenizes everything as a single {@link TokenType#WORD}. No escaping
+     * are possible. The '#' symbol denotes the start and end of this state.
+     */
     BASIC,
+
+    /**
+     * This state tokenizes the given input into {@link TokenType#WORD},
+     * {@link TokenType#NUMBER}, {@link TokenType#SYMBOL} tokens. It is possible to
+     * escape a backslash or a digit.
+     */
     EXTENDED
 }
+ 
