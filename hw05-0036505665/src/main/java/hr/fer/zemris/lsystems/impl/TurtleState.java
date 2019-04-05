d9cd4497dc2c9a7230ba8ee1deca47671ecf8469
Bruna <brdujmovic@gmail.com>
2019-04-05 23:19:56 +0200

    Add getters and setters

--- hw05-0036505665/src/main/java/hr/fer/zemris/lsystems/impl/TurtleState.java
+++ hw05-0036505665/src/main/java/hr/fer/zemris/lsystems/impl/TurtleState.java
@@ -3,6 +3,7 @@ package hr.fer.zemris.lsystems.impl;
 import hr.fer.zemris.math.Vector2D;
 
 import java.awt.*;
+import java.util.Objects;
 
 /**
  * This class represents a given turtle's current state - its position, direction,
@@ -48,6 +49,85 @@ public class TurtleState {
         this.currentPostition = currentPostition;
         this.direction = direction;
         this.color = color;
+        this.effectiveShiftLength = effectiveShiftLength;
+    }
+
+    /**
+     * Returns the current position of the turtle.
+     *
+     * @return the current position of the turtle
+     */
+    public Vector2D getCurrentPostition() {
+        return currentPostition;
+    }
+
+    /**
+     * Sets the current position of the turtle to a given vector.
+     *
+     * @throws NullPointerException if the given vector is {@code null}
+     */
+    public void setCurrentPostition(Vector2D currentPostition) {
+        Objects.requireNonNull(currentPostition);
+
+        this.currentPostition = currentPostition;
+    }
+
+    /**
+     * Returns the current direction of the turtle.
+     *
+     * @return the current direction of the turtle
+     */
+    public Vector2D getDirection() {
+        return direction;
+    }
+
+    /**
+     * Sets the current direction of the turtle to a given vector.
+     *
+     * @throws NullPointerException if the given vector is {@code null}
+     */
+    public void setDirection(Vector2D direction) {
+        this.direction = direction;
+    }
+
+    /**
+     * Returns the color in which the turtle draws.
+     *
+     * @return the color in which the turtle draws.
+     */
+    public Color getColor() {
+        return color;
+    }
+
+    /**
+     * Sets the color in which the turtle draws to a new value.
+     *
+     * @throws NullPointerException if the given color is {@code null}
+     */
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    /**
+     * Returns the current effective shift length of the turtle.
+     *
+     * @return the current effective shift length of the turtle
+     */
+    public double getEffectiveShiftLength() {
+        return effectiveShiftLength;
+    }
+
+    // TODO check IllegalArgExc
+    /**
+     * Sets the effective shift length to a given value.
+     *
+     * @throws IllegalArgumentException if the given shift length is less than 1
+     */
+    public void setEffectiveShiftLength(double effectiveShiftLength) {
+        if (effectiveShiftLength < 1) {
+            throw new IllegalArgumentException(
+                    "Effective shift length cannot be less than 1.");
+        }
         this.effectiveShiftLength = effectiveShiftLength;
     }
 
