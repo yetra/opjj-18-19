ac31f583c8b6c5380a729aba07c392461391dd36
Bruna <brdujmovic@gmail.com>
2019-04-07 00:43:21 +0200

    Add IComparisonOperator interface

---
+++ hw05-0036505665/src/main/java/hr/fer/zemris/java/hw05/db/IComparisonOperator.java
@@ -0,0 +1,19 @@
+package hr.fer.zemris.java.hw05.db;
+
+/**
+ * This functional interface is used for comparing two {@link String} values.
+ *
+ * @author Bruna Dujmović
+ * 
+ */
+public interface IComparisonOperator {
+
+    /**
+     * Returns {@code true} if the given values satisfy a specified comparison.
+     *
+     * @param value1 the first value to compare
+     * @param value2 the second value to compare
+     * @return {@code true} if the given values satisfy a specified comparison
+     */
+    public boolean satisfied(String value1, String value2);
+}
