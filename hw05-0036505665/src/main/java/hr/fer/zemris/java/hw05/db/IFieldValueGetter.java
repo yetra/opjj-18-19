1ed02878fb15a6fb2dcfb8a5be5ba42b9e5894d0
Bruna <brdujmovic@gmail.com>
2019-04-07 02:08:47 +0200

    Implement IFieldValueGetter interface

--- hw05-0036505665/src/main/java/hr/fer/zemris/java/hw05/db/IFieldValueGetter.java
+++ hw05-0036505665/src/main/java/hr/fer/zemris/java/hw05/db/IFieldValueGetter.java
@@ -1,4 +1,20 @@
 package hr.fer.zemris.java.hw05.db;
 
-public class IFieldValueGetter {
+/**
+ * This functional interface can return a specified field value from a student record in the
+ * database.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+@FunctionalInterface
+public interface IFieldValueGetter {
+
+    /**
+     * Returns a requested field value from a given student record.
+     *
+     * @param record the record from which the value will be returned
+     * @return a requested field value from a given student record
+     */
+    public String get(StudentRecord record);
 }
