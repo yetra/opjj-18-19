e89913abb0133fb3fb6d532c2860a8f07ba3d931
Bruna <brdujmovic@gmail.com>
2019-04-10 19:30:08 +0200

    Remove unused import

--- hw05-0036505665/src/main/java/hr/fer/zemris/lsystems/impl/commands/RotateCommand.java
+++ hw05-0036505665/src/main/java/hr/fer/zemris/lsystems/impl/commands/RotateCommand.java
@@ -3,7 +3,6 @@ package hr.fer.zemris.lsystems.impl.commands;
 import hr.fer.zemris.lsystems.Painter;
 import hr.fer.zemris.lsystems.impl.Command;
 import hr.fer.zemris.lsystems.impl.Context;
-import hr.fer.zemris.lsystems.impl.TurtleState;
 
 /**
  * This {@link Command} implementation changes the direction vector of a turtle's
