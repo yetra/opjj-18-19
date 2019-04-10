4a9321f6e6d5f2d70e99ded79c0c3a618891f726
Bruna <brdujmovic@gmail.com>
2019-04-10 20:48:21 +0200

    Rename queryExpression field and add missing javadoc

--- hw05-0036505665/src/main/java/hr/fer/zemris/java/hw05/db/QueryParser.java
+++ hw05-0036505665/src/main/java/hr/fer/zemris/java/hw05/db/QueryParser.java
@@ -8,28 +8,47 @@ import hr.fer.zemris.java.hw05.db.lexer.TokenType;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class represents a parser of query strings. Each query string is passed to
+ * the parser through its constructor and is separated into a list of
+ * {@link ConditionalExpression} objects.
+ *
+ * @author Bruna Dujmović
+ *
+ */
 public class QueryParser {
 
-    private String queryExpression;
+    /**
+     * The query string to parse.
+     */
+    private String query;
 
+    /**
+     * A list of {@link ConditionalExpression} objects that the given query string was
+     * parsed into.
+     */
     private List<ConditionalExpression> conditionalExpressions;
 
-    public QueryParser(String queryExpression) {
-        this.queryExpression = queryExpression;
+    /**
+     * Construct a {@link QueryParser} object for the given query string, and parses
+     * the string into {@link ConditionalExpression} objects.
+     *
+     * @param query the query to parse
+     */
+    public QueryParser(String query) {
+        this.query = query;
         this.conditionalExpressions = new ArrayList<>();
 
         parseQuery();
     }
 
-    private void parseQuery() {
-        try {
-            addExpressionsToList();
-        } catch (QueryLexerException e) {
-            System.out.println("Invalid query structure!");
-            System.exit(1);
-        }
-    }
-
+    /**
+     * Returns {@code true} if the current query is a direct query. A direct query is
+     * any query of the form jmbag="xxx" (i.e. it must have only one comparison of the
+     * attribute jmbag, and the operator must be equals)
+     *
+     * @return {@code true} if the current query is a direct query
+     */
     public boolean isDirectQuery() {
         if (conditionalExpressions.size() == 1) {
             ConditionalExpression expression = conditionalExpressions.get(0);
@@ -37,9 +56,19 @@ public class QueryParser {
             return expression.getFieldGetter() == FieldValueGetters.JMBAG // TODO ==?
                     && expression.getComparisonOperator() == ComparisonOperators.EQUALS;
         }
+
         return false;
     }
 
+    /**
+     * Returns the string literal that was compared to the jmbag attribute of the
+     * given direct query. If the given query was not direct, this method will throw
+     * the {@link IllegalStateException} exception.
+     *
+     * @return the string literal that was compared to the jmbag attribute of the
+     *         given direct query
+     * @throws IllegalStateException if the given query was not direct
+     */
     public String getQueriedJMBAG() {
         if (!isDirectQuery()) {
             throw new IllegalStateException("Query is not a direct query!");
@@ -48,12 +77,40 @@ public class QueryParser {
         return conditionalExpressions.get(0).getStringLiteral();
     }
 
+    /**
+     * Returns a list of conditional expressions parsed from the given query.
+     *
+     * @return a list of conditional expressions parsed from the given query
+     */
     public List<ConditionalExpression> getQuery() {
         return conditionalExpressions;
     }
 
+    /* ----------------------------------------------------------------------------
+     * ------------------------------ HELPER METHODS ------------------------------
+     * ----------------------------------------------------------------------------
+     */
+
+    /**
+     * Parses a given query string.
+     */
+    private void parseQuery() {
+        try {
+            addExpressionsToList();
+        } catch (QueryLexerException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Separates a given query into {@link ConditionalExpression} objects and adds
+     * them to the {@link #conditionalExpressions} list.
+     *
+     * @throws IllegalArgumentException if the given query is invalid
+     */
     private void addExpressionsToList() {
-        QueryLexer lexer = new QueryLexer(queryExpression);
+        QueryLexer lexer = new QueryLexer(query);
 
         do {
             Token fieldToken = lexer.nextToken();
@@ -90,6 +147,16 @@ public class QueryParser {
         } while (lexer.getToken().getType() != TokenType.EOF);
     }
 
+    /**
+     * Returns an {@link IFieldValueGetter} instance parsed from a given field value
+     * name string.
+     *
+     * @param fieldName the field value name to parse
+     * @return an {@link IFieldValueGetter} instance parsed from a given field value
+     *         name string
+     * @throws IllegalArgumentException if the given field value name string cannot be
+     *         parsed into a {@link IFieldValueGetter} object
+     */
     private IFieldValueGetter getFieldValueGetter(String fieldName) {
         switch (fieldName) {
             case "firstName":
@@ -104,6 +171,16 @@ public class QueryParser {
         }
     }
 
+    /**
+     * Returns an {@link IComparisonOperator} instance parsed from a given operator
+     * string.
+     *
+     * @param operator the operator string to parse
+     * @return an {@link IComparisonOperator} instance parsed from a given operator
+     *         string
+     * @throws IllegalArgumentException if the given operator string cannot be parsed
+     *         into a {@link IComparisonOperator} object
+     */
     private IComparisonOperator getComparisonOperator(String operator) {
         switch (operator) {
             case "<":
