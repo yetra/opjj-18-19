99ebb9577081bfb520becba755ffb317e48b6587
Bruna <brdujmovic@gmail.com>
2019-04-15 00:11:04 +0200

    Add javadoc for Crypto, main, checksha

--- hw06-0036505665/src/main/java/hr/fer/zemris/java/hw06/crypto/Crypto.java
+++ hw06-0036505665/src/main/java/hr/fer/zemris/java/hw06/crypto/Crypto.java
@@ -9,6 +9,20 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
+/**
+ * This is a program that allows the user to encrypt/decrypt a given file using the
+ * AES crypto-algorithm and a 128-bit encryption key, or calculate and check the
+ * SHA-256 file digest.
+ *
+ * The desired command is given through the command-line arguments. Valid commands
+ * are:
+ * checksha file_path - checks the SHA-256 digest of the file specified by file_path
+ * encrypt src_file_path dest_file_path - encrypts the given file to dest_file_path
+ * decrypt src_file_path dest_file_path - decrypts the given file to dest_file_path
+ *
+ * @author Bruna Dujmović
+ * 
+ */
 public class Crypto {
 
     /**
@@ -21,6 +35,13 @@ public class Crypto {
      */
     private static final String ALGORITHM = "SHA-256";
 
+    /**
+     * The main method. Parses the command given in the command-line arguments,
+     * prompts the user for further information through the console, and then
+     * prints the final results of the command.
+     *
+     * @param args the command-line arguments which represent
+     */
     public static void main(String[] args) {
 
         try (Scanner sc = new Scanner(System.in)) {
@@ -65,6 +86,13 @@ public class Crypto {
         }
     }
 
+    /**
+     * Checks if the calculated SHA-256 file digest matches the given digest and
+     * prints the results to the console.
+     *
+     * @param fileName the file whose SHA-256 digest will be checked
+     * @param expectedSha the expected SHA-256 digest
+     */
     private static void checksha(String fileName, String expectedSha) {
         String actualSha = getDigestOf(fileName);
 
@@ -77,6 +105,14 @@ public class Crypto {
         }
     }
 
+    /**
+     * Calculates the digest of a given file and returns it as a hex string.
+     *
+     * @param fileName the file whose digest will be calculated
+     * @return the hex string representation of the calculated digest
+     * @throws NullPointerException if the obtained digest cannot be represented
+     *         as a hex string
+     */
     private static String getDigestOf(String fileName) {
         try {
             MessageDigest messageDigest = MessageDigest.getInstance(ALGORITHM);
