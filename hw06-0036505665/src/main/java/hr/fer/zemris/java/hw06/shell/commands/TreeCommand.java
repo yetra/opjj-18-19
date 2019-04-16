7877dc6ce795dea02d8c9ab810ed34d4a1fdc2dd
Bruna <brdujmovic@gmail.com>
2019-04-17 01:38:36 +0200

    Add TreeCommand implementation

---
+++ hw06-0036505665/src/main/java/hr/fer/zemris/java/hw06/shell/commands/TreeCommand.java
@@ -0,0 +1,116 @@
+package hr.fer.zemris.java.hw06.shell.commands;
+
+import hr.fer.zemris.java.hw06.shell.Environment;
+import hr.fer.zemris.java.hw06.shell.ShellCommand;
+import hr.fer.zemris.java.hw06.shell.ShellStatus;
+import hr.fer.zemris.java.hw06.shell.Utility;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * This class represents the tree command that prints a recursive, tree-like listing
+ * of a given directory to the console. It accepts one argument - the path to the
+ * directory.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class TreeCommand implements ShellCommand {
+
+    @Override
+    public ShellStatus executeCommand(Environment env, String arguments) {
+        String[] parsed = Utility.parseArguments(arguments);
+
+        if (parsed.length != 1) {
+            env.writeln("Tree accepts one argument, " + parsed.length + " were given.");
+            return ShellStatus.CONTINUE;
+        }
+
+        try {
+            Path directoryPath = Paths.get(parsed[0]);
+
+            if (directoryPath.toFile().isDirectory()) {
+                Files.walkFileTree(directoryPath, new TreeFileVisitor(env));
+            } else {
+                env.writeln("The given file is not a directory.");
+            }
+
+        } catch (InvalidPathException e) {
+            env.writeln("Illegal path string \"" + parsed[0] + "\".");
+        } catch (IOException e) {
+            env.writeln("I/O error occurred.");
+        }
+
+        return ShellStatus.CONTINUE;
+    }
+
+    @Override
+    public String getCommandName() {
+        return "tree";
+    }
+
+    @Override
+    public List<String> getCommandDescription() {
+        return List.of("tree directory_path",
+                "directory_path -- path to the directory to be listed",
+                "Prints a recursive, tree-like listing of the specified directory."
+        );
+    }
+
+    /**
+     * A {@link FileVisitor} implementation that will print a tree-like listing of
+     * the given root directory.
+     */
+    private class TreeFileVisitor extends SimpleFileVisitor<Path> {
+
+        /**
+         * The {@link Environment} to be used for printing the listing.
+         */
+        private Environment env;
+
+        /**
+         * The current depth of the directory tree.
+         */
+        private int depth = 0;
+
+        /**
+         * Constructs a new {@link TreeFileVisitor} object with a given
+         * {@link Environment}.
+         *
+         * @param env the {@link Environment} to be used for printing the listing
+         * @throws NullPointerException if the given {@link Environment} is {@code null}
+         */
+        TreeFileVisitor(Environment env) {
+            this.env = Objects.requireNonNull(env);
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                throws IOException {
+            env.writeln("  ".repeat(depth) + dir.getFileName().toString());
+            depth++;
+
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                throws IOException {
+            env.writeln("  ".repeat(depth) + file.getFileName().toString());
+
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc)
+                throws IOException {
+            depth--;
+
+            return FileVisitResult.CONTINUE;
+        }
+    }
+}
