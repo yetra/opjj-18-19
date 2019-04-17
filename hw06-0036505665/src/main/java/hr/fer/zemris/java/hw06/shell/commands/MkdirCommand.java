6aa27d5e44575ab4e9e9d49e6250b995065cdeca
Bruna <brdujmovic@gmail.com>
2019-04-17 02:27:57 +0200

    Add MkdriCommand implementation

---
+++ hw06-0036505665/src/main/java/hr/fer/zemris/java/hw06/shell/commands/MkdirCommand.java
@@ -0,0 +1,60 @@
+package hr.fer.zemris.java.hw06.shell.commands;
+
+import hr.fer.zemris.java.hw06.shell.Environment;
+import hr.fer.zemris.java.hw06.shell.ShellCommand;
+import hr.fer.zemris.java.hw06.shell.ShellStatus;
+import hr.fer.zemris.java.hw06.shell.Utility;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * This class represents the mkdir command creates the appropriate directory structure
+ * at a specified location. It accepts one argument - the location (path) of the
+ * directory structure that will be created.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public class MkdirCommand implements ShellCommand {
+
+    @Override
+    public ShellStatus executeCommand(Environment env, String arguments) {
+        String[] parsed = Utility.parseArguments(arguments);
+
+        if (parsed.length != 1) {
+            env.writeln("Mkdir accepts one argument, " + parsed.length + " were given.");
+            return ShellStatus.CONTINUE;
+        }
+
+        try {
+            Path directoryPath = Paths.get(parsed[0]);
+
+            Files.createDirectories(directoryPath);
+
+        } catch (InvalidPathException e) {
+            env.writeln("Illegal path string \"" + parsed[0] + "\".");
+        } catch (IOException e) {
+            env.writeln("I/O error occurred.");
+        }
+
+        return ShellStatus.CONTINUE;
+    }
+
+    @Override
+    public String getCommandName() {
+        return "mkdir";
+    }
+
+    @Override
+    public List<String> getCommandDescription() {
+        return List.of("mkdir dir_path",
+                "dir_path -- the location (path) of the directory structure that " +
+                        "will be created",
+                "Creates the appropriate directory structure at the specified location.");
+    }
+}
