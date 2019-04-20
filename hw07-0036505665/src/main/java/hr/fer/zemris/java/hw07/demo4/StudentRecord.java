e93447267334ac87dc40ff931f88f65c450207b8
Bruna <brdujmovic@gmail.com>
2019-04-21 01:54:10 +0200

    Implement StudentRecord class

---
+++ hw07-0036505665/src/main/java/hr/fer/zemris/java/hw07/demo4/StudentRecord.java
@@ -0,0 +1,70 @@
+package hr.fer.zemris.java.hw07.demo4;
+
+import java.util.Objects;
+
+public class StudentRecord {
+
+    /**
+     * The jmbag of the student.
+     */
+    private String jmbag;
+
+    /**
+     * The last name of the student.
+     */
+    private String lastName;
+
+    /**
+     * The first name of the student.
+     */
+    private String firstName;
+
+    /**
+     * The midterm exam score of the student.
+     */
+    private int midtermScore;
+
+    /**
+     * The final exam score of the student.
+     */
+    private int finalScore;
+
+    /**
+     * The lab score of the student.
+     */
+    private int labScore;
+
+    /**
+     * The grade of the student.
+     */
+    private int grade;
+
+    /**
+     * Constructs a new {@link StudentRecord} given all the student data.
+     *
+     * @param jmbag the jmbag of the student
+     * @param lastName the last name of the student
+     * @param firstName the first name of the student
+     * @param midtermScore the midterm exam score of the student
+     * @param finalScore the final exam score of the student
+     * @param labScore the lab score of the student
+     * @param grade the grade of the student
+     * @throws NullPointerException if the given jmbag, last name, or first name are
+     *         {@code null}
+     * @throws IllegalArgumentException if the given scores or grade are negative
+     */
+    public StudentRecord(String jmbag, String lastName, String firstName,
+                         int midtermScore, int finalScore, int labScore, int grade) {
+        if (midtermScore < 0 || finalScore < 0 || labScore < 0 || grade < 0) {
+            throw new IllegalArgumentException("Scores and grade cannot be negative.");
+        }
+
+        this.jmbag = Objects.requireNonNull(jmbag);
+        this.lastName = Objects.requireNonNull(lastName);
+        this.firstName = Objects.requireNonNull(firstName);
+        this.midtermScore = midtermScore;
+        this.finalScore = finalScore;
+        this.labScore = labScore;
+        this.grade = grade;
+    }
+}
