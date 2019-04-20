8b187e792c1a3c6d29b157a4cc370327ac07e42b
Bruna <brdujmovic@gmail.com>
2019-04-20 23:13:24 +0200

    Write javadoc for the IntegerStorageObserver interface

--- hw07-0036505665/src/main/java/hr/fer/zemris/java/hw07/observer1/IntegerStorageObserver.java
+++ hw07-0036505665/src/main/java/hr/fer/zemris/java/hw07/observer1/IntegerStorageObserver.java
@@ -1,6 +1,19 @@
 package hr.fer.zemris.java.hw07.observer1;
 
+/**
+ * This class models an observer that listens for a change in an {@link IntegerStorage}
+ * object's stored value.
+ *
+ * @author Bruna Dujmović
+ *
+ */
 public interface IntegerStorageObserver {
 
+    /**
+     * The method to execute when a given {@link IntegerStorage} object's stored
+     * value changes.
+     *
+     * @param istorage the {@link IntegerStorage} object whose stored value changed
+     */
     public void valueChanged(IntegerStorage istorage);
 }
