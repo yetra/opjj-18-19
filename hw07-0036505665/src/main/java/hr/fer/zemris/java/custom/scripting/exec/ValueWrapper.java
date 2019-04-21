ab2eac9ade4e61b892681e5fc7ae93303f1feefb
Bruna <brdujmovic@gmail.com>
2019-04-21 12:30:55 +0200

    Add constructor, getter and setter for ValueWrapper

--- hw07-0036505665/src/main/java/hr/fer/zemris/java/custom/scripting/exec/ValueWrapper.java
+++ hw07-0036505665/src/main/java/hr/fer/zemris/java/custom/scripting/exec/ValueWrapper.java
@@ -1,17 +1,42 @@
 package hr.fer.zemris.java.custom.scripting.exec;
 
+/**
+ * This class represents a wrapper of a given object value. It supports arithmetic
+ * operations on values that can be parsed to {@code double} or {@code int}.
+ *
+ * @author Bruna Dujmović
+ * 
+ */
 public class ValueWrapper {
 
+    /**
+     * The value object that is wrapped by this {@link ValueWrapper}.
+     */
     private Object value;
 
+    /**
+     * Constructs a {@link ValueWrapper} for the given value.
+     *
+     * @param value the value object to wrap
+     */
     public ValueWrapper(Object value) {
         this.value = value;
     }
 
+    /**
+     * Returns the value that is wrapped by this {@link ValueWrapper}.
+     *
+     * @return the value that is wrapped by this {@link ValueWrapper}
+     */
     public Object getValue() {
         return value;
     }
 
+    /**
+     * Sets the value wrapped by this {@link ValueWrapper} to the given new value.
+     *
+     * @param value the new value to wrap
+     */
     public void setValue(Object value) {
         this.value = value;
     }
