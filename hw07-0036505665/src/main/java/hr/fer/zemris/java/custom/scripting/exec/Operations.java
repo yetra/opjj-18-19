a3a2d23c735fa3f616b0416a07de61e3f4120aac
Bruna <brdujmovic@gmail.com>
2019-04-24 23:08:10 +0200

    Add javadoc for Operations class

--- hw07-0036505665/src/main/java/hr/fer/zemris/java/custom/scripting/exec/Operations.java
+++ hw07-0036505665/src/main/java/hr/fer/zemris/java/custom/scripting/exec/Operations.java
@@ -2,14 +2,33 @@ package hr.fer.zemris.java.custom.scripting.exec;
 
 import java.util.function.BinaryOperator;
 
+/**
+ * This class contains {@link BinaryOperator}s that represent all possible arithmetic
+ * operations on {@link ValueWrapper} objects.
+ *
+ * @author Bruna Dujmović
+ *
+ */
 class Operations {
 
+    /**
+     * Addition of two Double values.
+     */
     static final BinaryOperator<Double> ADD = (first, second) -> first + second;
 
+    /**
+     * Subtraction of two Double values.
+     */
     static final BinaryOperator<Double> SUB = (first, second) -> first - second;
 
+    /**
+     * Multiplication of two Double values.
+     */
     static final BinaryOperator<Double> MUL = (first, second) -> first * second;
 
+    /**
+     * Division of two Double values.
+     */
     static final BinaryOperator<Double> DIV = (first, second) -> first / second;
 
 }
