fe03318918ac0f4e9d6eb30fc2404871245f3a57
Bruna <brdujmovic@gmail.com>
2019-05-01 01:58:50 +0200

    Optimize the bfs-like state-space search algorithm

--- hw08-0036505665/src/main/java/searching/algorithms/SearchUtil.java
+++ hw08-0036505665/src/main/java/searching/algorithms/SearchUtil.java
@@ -1,7 +1,9 @@
 package searching.algorithms;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -43,6 +45,51 @@ public class SearchUtil {
                 toExplore.add(new Node<>(
                         node, transition.getState(), node.getCost() + transition.getCost()
                 ));
+            }
+        }
+
+        return node;
+    }
+
+    /**
+     * Returns the solution represented by a {@link Node} object for a given problem
+     * defined as the tuple (s0, succ(s), goal(s)) using an optimized breadth-first
+     * search algorithm.
+     *
+     * This implementation keeps track of already visited states and doesn't add them
+     * again to the list of states to explore.
+     *
+     * @param s0 the initial state
+     * @param succ a {@link Function} that returns a list of neighboring states for a
+     *             given state
+     * @param goal a {@link Predicate} that returns {@code false} if a given state
+     *             does not match the goal state
+     * @param <S> the type of the states
+     * @return he solution represented by a {@link Node} object for a given problem
+     */
+    public static <S> Node<S> bfsv(Supplier<S> s0, Function<S, List<Transition<S>>> succ,
+                                  Predicate<S> goal) {
+        Set<S> visited = new HashSet<>();
+        List<Node<S>> toExplore = new LinkedList<>();
+        Node<S> node = null;
+
+        toExplore.add(new Node<>(null, s0.get(), 0));
+        visited.add(s0.get());
+
+        while (!toExplore.isEmpty()) {
+            node = toExplore.remove(0);
+
+            if (goal.test(node.getState())) {
+                return node;
+            }
+
+            List<Transition<S>> transitions = succ.apply(node.getState());
+            for (Transition<S> transition : transitions) {
+
+                if (visited.add(transition.getState())) {
+                    toExplore.add(new Node<>(node, transition.getState(),
+                            node.getCost() + transition.getCost()));
+                }
             }
         }
 
