0a2a49d71a3a7ba0aced2ce2a720804c1e916740
Bruna <brdujmovic@gmail.com>
2019-04-30 23:16:31 +0200

    Add demo for bfs method

---
+++ hw08-0036505665/src/main/java/coloring/demo/Bojanje2.java
@@ -0,0 +1,31 @@
+package coloring.demo;
+
+import coloring.algorithms.Coloring;
+import coloring.algorithms.Pixel;
+import coloring.algorithms.SubspaceExploreUtil;
+import marcupic.opjj.statespace.coloring.FillAlgorithm;
+import marcupic.opjj.statespace.coloring.FillApp;
+import marcupic.opjj.statespace.coloring.Picture;
+
+import java.util.Arrays;
+
+public class Bojanje2 {
+
+    public static void main(String[] args) {
+        FillApp.run(FillApp.OWL, Arrays.asList(bfs));
+    }
+
+    private static final FillAlgorithm bfs = new FillAlgorithm() {
+
+        @Override
+        public String getAlgorithmTitle() {
+            return "Moj bfs!";
+        }
+
+        @Override
+        public void fill(int x, int y, int color, Picture picture) {
+            Coloring col = new Coloring(new Pixel(x,y), picture, color);
+            SubspaceExploreUtil.bfs(col.s0, col.process, col.succ, col.acceptable);
+        }
+    };
+}
