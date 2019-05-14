9b3107cbcb32e18766466d56c8205b9acf3e1862
Bruna <brdujmovic@gmail.com>
2019-05-14 23:35:02 +0200

    Split chart painting into methods

--- hw10-0036505665/src/main/java/hr/fer/zemris/java/gui/charts/BarChartComponent.java
+++ hw10-0036505665/src/main/java/hr/fer/zemris/java/gui/charts/BarChartComponent.java
@@ -15,22 +15,65 @@ public class BarChartComponent extends JComponent {
     /**
      * The space between the axis and the caption.
      */
-    private static final int AXIS_SPACING = 20;
-
+    private static final int CHART_SPACING = 20;
     /**
      * The line between the last tick mark and the end of the arrow.
      */
     private static final int AXIS_EXTENSION = 5;
-
     /**
      * The length of a tick mark on the axis.
      */
     private static final int TICK_LENGTH = 8;
 
     /**
+     * The color of the axes.
+     */
+    private static final Color AXIS_COLOR = new Color(0xB4B2B2);
+    /**
+     * The thickness of the axis lines.
+     */
+    private static final BasicStroke AXIS_STROKE = new BasicStroke(2);
+
+    /**
+     * The color of the grid.
+     */
+    private static final Color GRID_COLOR = new Color(0xF2E6D2);
+    /**
+     * The thickness of the grid lines.
+     */
+    private static final BasicStroke GRID_STROKE = new BasicStroke(2);
+
+    /**
+     * The fill color of the chart bars.
+     */
+    private static final Color BAR_FILL = new Color(0xE78154);
+    /**
+     * The outline color of the chart bars.
+     */
+    private static final Color BAR_OUTLINE = Color.WHITE;
+    /**
+     * The thickness of the bar outline.
+     */
+    private static final BasicStroke BAR_OUTLINE_STROKE = new BasicStroke(1);
+
+    /**
      * The chart to paint.
      */
     private BarChart chart;
+
+    /**
+     * A container for x-axis data.
+     */
+    private Axis xAxis;
+    /**
+     * A container for y-axis data.
+     */
+    private Axis yAxis;
+
+    /**
+     * The dimensions of a grid cell.
+     */
+    private Dimension cell;
 
     /**
      * Constructs a {@link BarChartComponent} for the given chart.
@@ -39,6 +82,9 @@ public class BarChartComponent extends JComponent {
      */
     public BarChartComponent(BarChart chart) {
         this.chart = chart;
+
+        setOpaque(true);
+        setBackground(Color.WHITE);
     }
 
     @Override
@@ -46,101 +92,198 @@ public class BarChartComponent extends JComponent {
         Graphics2D g2d = (Graphics2D) g;
         List<XYValue> values = chart.getValues();
 
-        Dimension size = getSize();
+        Dimension component = getSize();
         Insets insets = getInsets();
         FontMetrics fm = g2d.getFontMetrics();
 
-        g2d.setStroke(new BasicStroke(2));
-        g2d.setColor(Color.GRAY);
-
         int maxYNumberWidth = fm.stringWidth(Integer.toString(chart.getMaxY()));
 
-        int xAxisStart = insets.left + fm.getHeight() + AXIS_SPACING + maxYNumberWidth + AXIS_SPACING;
-        int xAxisEnd = size.width - insets.right;
-
-        int yAxisStart = size.height - (insets.bottom + fm.getHeight() + AXIS_SPACING + fm.getHeight() + AXIS_SPACING);
-        int yAxisEnd = insets.top;
-
-        int rowHeight = (yAxisStart - yAxisEnd) * chart.getySpacing() / (chart.getMaxY() - chart.getMinY());
-        int columnWidth = (xAxisEnd - xAxisStart) / values.size();
-
-        // X-AXIS
-        // axis line
-        g2d.drawLine(xAxisStart, yAxisStart, xAxisEnd, yAxisStart);
-        // initial tick
-        g2d.drawLine(xAxisStart, yAxisStart, xAxisStart, yAxisStart + TICK_LENGTH);
-        // x-axis caption
-        g2d.drawString(
-                chart.getxCaption(),
-                xAxisStart + (xAxisEnd - AXIS_EXTENSION - xAxisStart) / 2 - fm.stringWidth(chart.getxCaption()) / 2,
-                yAxisStart + AXIS_SPACING + fm.getHeight() + AXIS_SPACING
+        xAxis = new Axis(
+                insets.left + fm.getHeight() + CHART_SPACING + maxYNumberWidth + CHART_SPACING,
+                component.width - insets.right
         );
-        for (int col = 1, colCount = values.size(); col <= colCount; col++) {
-            // x-axis number
-            String xNumber = Integer.toString(values.get(col - 1).getX());
-            g2d.drawString(
-                    xNumber,
-                    xAxisStart + col * columnWidth - columnWidth / 2 - fm.stringWidth(xNumber) / 2,
-                    yAxisStart + AXIS_SPACING
-            );
 
-            // tick mark
-            g2d.drawLine(
-                    xAxisStart + col * columnWidth, yAxisStart,
-                    xAxisStart + col * columnWidth, yAxisStart + TICK_LENGTH
-            );
+        yAxis = new Axis(
+                component.height - (insets.bottom + fm.getHeight() + CHART_SPACING + fm.getHeight() + CHART_SPACING),
+                insets.top
+        );
 
-            // vertical grid line
+        cell = new Dimension(
+                (xAxis.end - xAxis.start) / values.size(),
+                (yAxis.start - yAxis.end) * chart.getySpacing() / (chart.getMaxY() - chart.getMinY())
+        );
+
+        paintGrid(g2d);
+        paintBars(g2d);
+        paintXAxis(g2d, fm);
+        paintYAxis(g2d, fm);
+
+        g2d.setStroke(new BasicStroke(2));
+        g2d.setColor(Color.GRAY);
+    }
+
+    /**
+     * Paints the grid lines of the bar chart.
+     *
+     * @param g2d the {@link Graphics2D} object to use
+     */
+    private void paintGrid(Graphics2D g2d) {
+        g2d.setStroke(GRID_STROKE);
+        g2d.setColor(GRID_COLOR);
+
+        // vertical grid lines
+        int xCount = chart.getValues().size();
+        for (int x = 1; x <= xCount; x++) {
             g2d.drawLine(
-                    xAxisStart + col * columnWidth, yAxisStart,
-                    xAxisStart + col * columnWidth, yAxisEnd + AXIS_EXTENSION
+                    xAxis.start + x * cell.width, yAxis.start,
+                    xAxis.start + x * cell.width, yAxis.end + AXIS_EXTENSION
             );
         }
-        g2d.drawLine(xAxisEnd - AXIS_EXTENSION, yAxisStart, xAxisEnd, yAxisStart);
 
-        // y-axis
-        int rowCount = (chart.getMaxY() - chart.getMinY()) / chart.getySpacing();
-        g2d.drawLine(xAxisStart, yAxisStart, xAxisStart, yAxisEnd);
-        g2d.drawLine(xAxisStart - TICK_LENGTH, yAxisStart, xAxisStart, yAxisStart);
-        for (int row = 1; row <= rowCount; row++) {
-            // y-axis number
-            String yNumber = Integer.toString(chart.getMinY() + (row - 1) * chart.getySpacing());
-            g2d.drawString(
-                    yNumber,
-                    xAxisStart - TICK_LENGTH - AXIS_SPACING - fm.stringWidth(yNumber),
-                    yAxisStart - (row - 1) * rowHeight + fm.getAscent() / 2 - 1 // brush stroke
-            );
-
-            // tick mark
+        // horizontal grid lines
+        int yCount = (chart.getMaxY() - chart.getMinY()) / chart.getySpacing();
+        for (int y = 1; y <= yCount; y++) {
             g2d.drawLine(
-                    xAxisStart - TICK_LENGTH, yAxisStart - row * rowHeight,
-                    xAxisStart, yAxisStart - row * rowHeight
-            );
-
-            // horizontal grid line
-            g2d.drawLine(
-                    xAxisStart, yAxisStart - row * rowHeight,
-                    xAxisEnd - AXIS_EXTENSION, yAxisStart - row * rowHeight
+                    xAxis.start, yAxis.start - y * cell.height,
+                    xAxis.end - AXIS_EXTENSION, yAxis.start - y * cell.height
             );
         }
-        g2d.drawLine(xAxisEnd - AXIS_EXTENSION, yAxisStart, xAxisEnd, yAxisStart);
+    }
 
-        // bars
+    /**
+     * Paints the bars of the bar chart.
+     *
+     * @param g2d the {@link Graphics2D} object to use
+     */
+    private void paintBars(Graphics2D g2d) {
+        List<XYValue> values = chart.getValues();
         int ySpacing = chart.getySpacing();
+
+        g2d.setStroke(BAR_OUTLINE_STROKE);
+
         for (int x = 0, maxX = values.size(); x < maxX; x++) {
-            int xValue = values.get(x).getX();
             int yValue = values.get(x).getY();
 
-            g2d.setColor(Color.ORANGE);
-            g2d.fillRect(
-                    xAxisStart + x * columnWidth , yAxisStart - rowHeight  * (yValue / ySpacing),
-                    columnWidth, rowHeight * (yValue / ySpacing)
+            Rectangle r = new Rectangle(
+                    xAxis.start + x * cell.width,
+                    yAxis.start - cell.height  * (yValue / ySpacing),
+                    cell.width,
+                    cell.height * (yValue / ySpacing)
             );
-            g2d.setColor(Color.WHITE);
-            g2d.drawRect(
-                    xAxisStart + x * columnWidth , yAxisStart - rowHeight  * (yValue / ySpacing),
-                    columnWidth, rowHeight * (yValue / ySpacing)
+
+            g2d.setColor(BAR_FILL);
+            g2d.fillRect(r.x, r.y, r.width, r.height);
+
+            g2d.setColor(BAR_OUTLINE);
+            g2d.drawRect(r.x, r.y, r.width, r.height);
+        }
+    }
+
+    /**
+     * Paints the x axis of the bar chart.
+     *
+     * @param g2d the {@link Graphics2D} object to use
+     * @param fm the corresponding {@link FontMetrics} object
+     */
+    private void paintXAxis(Graphics2D g2d, FontMetrics fm) {
+        List<XYValue> values = chart.getValues();
+        int xCount = values.size();
+        
+        g2d.setStroke(AXIS_STROKE);
+        g2d.setColor(AXIS_COLOR);
+        
+        // axis line
+        g2d.drawLine(xAxis.start, yAxis.start, xAxis.end, yAxis.start);
+        
+        // caption
+        g2d.drawString(
+                chart.getxCaption(),
+                xAxis.start + (xAxis.end - AXIS_EXTENSION - xAxis.start) / 2 - fm.stringWidth(chart.getxCaption()) / 2,
+                yAxis.start + CHART_SPACING + fm.getHeight() + CHART_SPACING
+        );
+        
+        // initial tick mark
+        g2d.drawLine(xAxis.start, yAxis.start, xAxis.start, yAxis.start + TICK_LENGTH);
+        
+        for (int x = 1; x <= xCount; x++) {
+            // value
+            String value = Integer.toString(values.get(x - 1).getX());
+            g2d.drawString(
+                    value,
+                    xAxis.start + x * cell.width - cell.width / 2 - fm.stringWidth(value) / 2,
+                    yAxis.start + CHART_SPACING
             );
+
+            // tick mark
+            g2d.drawLine(
+                    xAxis.start + x * cell.width, yAxis.start,
+                    xAxis.start + x * cell.width, yAxis.start + TICK_LENGTH
+            );
+        }
+        
+//        g2d.drawLine(xAxis.end - AXIS_EXTENSION, yAxis.start, xAxis.end, yAxis.start);
+    }
+
+    /**
+     * Paints the y axis of the bar chart.
+     *
+     * @param g2d the {@link Graphics2D} object to use
+     * @param fm the corresponding {@link FontMetrics} object
+     */
+    private void paintYAxis(Graphics2D g2d, FontMetrics fm) {
+        int yCount = (chart.getMaxY() - chart.getMinY()) / chart.getySpacing();
+
+        g2d.setStroke(AXIS_STROKE);
+        g2d.setColor(AXIS_COLOR);
+        
+        // axis line
+        g2d.drawLine(xAxis.start, yAxis.start, xAxis.start, yAxis.end);
+        
+        // initial tick mark
+        g2d.drawLine(xAxis.start - TICK_LENGTH, yAxis.start, xAxis.start, yAxis.start);
+        
+        for (int row = 1; row <= yCount; row++) {
+            // value
+            String value = Integer.toString(chart.getMinY() + (row - 1) * chart.getySpacing());
+            g2d.drawString(
+                    value,
+                    xAxis.start - TICK_LENGTH - CHART_SPACING - fm.stringWidth(value),
+                    yAxis.start - (row - 1) * cell.height + fm.getAscent() / 2 - 1 // brush stroke
+            );
+
+            // tick mark
+            g2d.drawLine(
+                    xAxis.start - TICK_LENGTH, yAxis.start - row * cell.height,
+                    xAxis.start, yAxis.start - row * cell.height
+            );
+        }
+        g2d.drawLine(xAxis.end - AXIS_EXTENSION, yAxis.start, xAxis.end, yAxis.start);
+    }
+
+    /**
+     * A helper class that serves as a container for the axes' start and end coordinates.
+     */
+    private class Axis {
+
+        /**
+         * The start coordinate of the axis.
+         */
+        private int start;
+
+        /**
+         * The end coordinate of the axis.
+         */
+        private int end;
+
+        /**
+         * Constructs an {@link Axis} of the given start and end coordinates.
+         *
+         * @param start the start coordinate of the axis
+         * @param end the end coordinate of the axis
+         */
+        public Axis(int start, int end) {
+            this.start = start;
+            this.end = end;
         }
     }
 }
