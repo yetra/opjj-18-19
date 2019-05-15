f1e85b5f91cbbe94290ff58f927adb9221b53317
Bruna <brdujmovic@gmail.com>
2019-05-15 23:20:28 +0200

    Parse XYValue from string

--- hw10-0036505665/src/main/java/hr/fer/zemris/java/gui/charts/XYValue.java
+++ hw10-0036505665/src/main/java/hr/fer/zemris/java/gui/charts/XYValue.java
@@ -45,4 +45,25 @@ public class XYValue {
     public int getY() {
         return y;
     }
+
+    /**
+     * Returns an {@link XYValue} object parsed from a given string.
+     *
+     * @param valueString the string to parse
+     * @return an {@link XYValue} object parsed from the given string
+     * @throws IllegalArgumentException if the given string isn't properly formatted
+     * @throws NumberFormatException if the string's elements cannot be parsed into Integers
+     */
+    public static XYValue fromString(String valueString) {
+        String[] parts = valueString.split(",");
+
+        if (parts.length != 2) {
+            throw new IllegalArgumentException("Invalid string format!");
+        }
+
+        int x = Integer.parseInt(parts[0]);
+        int y = Integer.parseInt(parts[1]);
+
+        return new XYValue(x, y);
+    }
 }
