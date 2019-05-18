42ce4031e4868353c825717f2208a2d54093536a
Bruna <brdujmovic@gmail.com>
2019-05-18 18:29:19 +0200

    Create JNotepadPP actions

--- hw11-0036505665/src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/JNotepadPP.java
+++ hw11-0036505665/src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/JNotepadPP.java
@@ -1,7 +1,12 @@
 package hr.fer.zemris.java.hw11.jnotepadpp;
 
 import javax.swing.*;
+import javax.swing.text.DefaultEditorKit;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * {@link JNotepadPP} is a simple text file editor that supports working with multiple
@@ -63,7 +68,36 @@ public class JNotepadPP extends JFrame {
      * subscribed GUI components.
      */
     private void createActions() {
+        newDocument.putValue(Action.NAME, "New");
+        newDocument.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke("control N"));
+        newDocument.putValue(Action.MNEMONIC_KEY, KeyEvent.VK_N);
+        newDocument.putValue(Action.SHORT_DESCRIPTION, "Create new document");
 
+        openDocument.putValue(Action.NAME, "Open");
+        openDocument.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke("control O"));
+        openDocument.putValue(Action.MNEMONIC_KEY, KeyEvent.VK_O);
+        openDocument.putValue(Action.SHORT_DESCRIPTION, "Open document from disc");
+
+        saveDocument.putValue(Action.NAME, "Save");
+        saveDocument.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke("control S"));
+        saveDocument.putValue(Action.MNEMONIC_KEY, KeyEvent.VK_S);
+        saveDocument.putValue(Action.SHORT_DESCRIPTION, "Save document to disc");
+
+        saveAsDocument.putValue(Action.NAME, "Save As");
+        saveAsDocument.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke("control shift S"));
+        saveAsDocument.putValue(Action.MNEMONIC_KEY, KeyEvent.VK_S);
+        saveAsDocument.putValue(Action.SHORT_DESCRIPTION, "Save document to disc");
+
+        closeDocument.putValue(Action.NAME, "Close");
+        closeDocument.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke("control W"));
+        closeDocument.putValue(Action.MNEMONIC_KEY, KeyEvent.VK_C);
+        closeDocument.putValue(Action.SHORT_DESCRIPTION, "Close current document without saving");
+
+        cutAction.putValue(Action.NAME, "Cut");
+
+        copyAction.putValue(Action.NAME, "Copy");
+
+        pasteAction.putValue(Action.NAME, "Paste");
     }
 
     /**
@@ -80,6 +114,178 @@ public class JNotepadPP extends JFrame {
      */
     private JToolBar createToolBar() {
         return null;
+    }
+
+    /*
+     * ---------------------------------------------------------------------------
+     * --------------------------------- Actions ---------------------------------
+     * ---------------------------------------------------------------------------
+     */
+
+    /**
+     * Creates a new blank document.
+     */
+    private final Action newDocument = new AbstractAction() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            mdm.createNewDocument();
+        }
+    };
+
+    /**
+     * Opens an existing document.
+     */
+    private final Action openDocument = new AbstractAction() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            openDocument();
+        }
+    };
+
+    /**
+     * Saves the current document.
+     */
+    private final Action saveDocument = new AbstractAction() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            saveDocument(false);
+        }
+    };
+
+    /**
+     * Performs save-as on the current document.
+     */
+    private final Action saveAsDocument = new AbstractAction() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            saveDocument(true);
+        }
+    };
+
+    /**
+     * Closes the current document.
+     */
+    private final Action closeDocument = new AbstractAction() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            mdm.closeDocument(mdm.getCurrentDocument());
+        }
+    };
+
+    /**
+     * Cuts the selected text and places its contents into the system clipboard.
+     */
+    private final Action cutAction = new DefaultEditorKit.CutAction();
+
+    /**
+     * Copies the contents of the selected text to the system clipboard.
+     */
+    private final Action copyAction = new DefaultEditorKit.CopyAction();
+
+    /**
+     * Pastes the contents of the system clipboard onto the selection or before the
+     * caret, if nothing is selected.
+     */
+    private final Action pasteAction = new DefaultEditorKit.PasteAction();
+
+    /*
+     * ---------------------------------------------------------------------------
+     * ----------------------------- Action methods ------------------------------
+     * ---------------------------------------------------------------------------
+     */
+
+    /**
+     * A helper method for performing the {@link #openDocument} action.
+     */
+    private void openDocument() {
+        JFileChooser jfc = new JFileChooser();
+        jfc.setDialogTitle("Open file");
+        if (jfc.showOpenDialog(this) != JFileChooser.APPROVE_OPTION) {
+            return;
+        }
+
+        Path filePath = jfc.getSelectedFile().toPath();
+        if (!Files.isReadable(filePath)) {
+            JOptionPane.showMessageDialog(
+                    this,
+                    "The file " + filePath + " is not readable!",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
+            return;
+        }
+
+        try {
+            mdm.loadDocument(filePath);
+
+        } catch (IllegalArgumentException exc) {
+            JOptionPane.showMessageDialog(
+                    this,
+                    exc.getMessage(),
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
+    }
+
+    /**
+     * A helper method for performing the {@link #saveDocument} and {@link #saveAsDocument}
+     * actions.
+     *
+     * @param isSaveAs {@code true} if save-as is to be performed
+     */
+    private void saveDocument(boolean isSaveAs) {
+        Path savePath = mdm.getCurrentDocument().getFilePath();
+
+        if (isSaveAs || savePath == null) {
+            JFileChooser jfc = new JFileChooser();
+            jfc.setDialogTitle("Save document");
+            if (jfc.showSaveDialog(this) != JFileChooser.APPROVE_OPTION) {
+                JOptionPane.showMessageDialog(
+                        this,
+                        "Nothing was saved!",
+                        "Save Document Aborted",
+                        JOptionPane.INFORMATION_MESSAGE
+                );
+                return;
+            }
+
+            savePath = jfc.getSelectedFile().toPath();
+            if (Files.exists(savePath)) {
+                int response = JOptionPane.showConfirmDialog(
+                        this,
+                        "A file already exists on the path " + savePath +
+                                ".\nDo you wish to overwrite it?",
+                        "Target File Already Exists",
+                        JOptionPane.YES_NO_OPTION
+                );
+
+                if (response == JOptionPane.NO_OPTION) {
+                    JOptionPane.showMessageDialog(
+                            this,
+                            "Nothing was saved!",
+                            "Save Document Aborted",
+                            JOptionPane.INFORMATION_MESSAGE
+                    );
+                    return;
+                }
+            }
+        }
+
+        try {
+            SingleDocumentModel modelToSave = mdm.getCurrentDocument();
+            mdm.saveDocument(modelToSave, savePath);
+            modelToSave.setModified(false);
+            modelToSave.setFilePath(savePath);
+
+        } catch (IllegalArgumentException e) {
+            JOptionPane.showMessageDialog(
+                    this,
+                    e.getMessage(),
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
     }
 
     /*
