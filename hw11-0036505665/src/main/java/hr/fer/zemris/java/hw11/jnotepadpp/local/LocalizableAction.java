ba737c964b51b10c2b35e11783a6d3b4008bbb83
Bruna <brdujmovic@gmail.com>
2019-05-20 17:59:30 +0200

    Create a LocalizableAction class

---
+++ hw11-0036505665/src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/local/LocalizableAction.java
@@ -0,0 +1,52 @@
+package hr.fer.zemris.java.hw11.jnotepadpp.local;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+/**
+ * An extension of {@link AbstractAction} that updates its {@link #NAME} key and
+ * corresponding translation value on each localization change event.
+ *
+ * @author Bruna Dujmović
+ *
+ */
+public abstract class LocalizableAction extends AbstractAction {
+
+    /**
+     * The version number of this serializable class.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The translation key.
+     */
+    private String key;
+
+    /**
+     * The {@link ILocalizationProvider} for translating the {@link #key}.
+     */
+    private ILocalizationProvider lp;
+
+    /**
+     * Updates and stores the translation for the {@link #key}.
+     */
+    private final ILocalizationListener listener = () -> putValue(NAME, lp.getString(key));
+
+    /**
+     * Constructs a {@link LocalizableAction} for the specified key and registers a
+     * listener for localization changes on the given {@link ILocalizationProvider}.
+     *
+     * @param key the translation key
+     * @param lp the {@link ILocalizationProvider} for translating the {@link #key}
+     */
+    public LocalizableAction(String key, ILocalizationProvider lp) {
+        this.key = key;
+        this.lp = lp;
+
+        putValue(NAME, lp.getString(key));
+        lp.addLocalizationListener(listener);
+    }
+
+    @Override
+    public abstract void actionPerformed(ActionEvent e);
+}
