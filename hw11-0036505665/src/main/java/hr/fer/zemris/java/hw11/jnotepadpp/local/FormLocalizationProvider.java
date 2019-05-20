3b08946d1471f9442ca48974617e06b78f8c8c8a
Bruna <brdujmovic@gmail.com>
2019-05-20 17:38:48 +0200

    Connect and disconnect bridge on window open/close

---
+++ hw11-0036505665/src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/local/FormLocalizationProvider.java
@@ -0,0 +1,40 @@
+package hr.fer.zemris.java.hw11.jnotepadpp.local;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * A class derived from {@link LocalizationProviderBridge} that is registered as a
+ * {@link java.awt.event.WindowListener} to the given {@link JFrame} so it can be
+ * connected when frame is opened and disconnected when frame is closed.
+ *
+ * @see LocalizationProviderBridge
+ * @author Bruna Dujmović
+ *
+ */
+public class FormLocalizationProvider extends LocalizationProviderBridge {
+
+    /**
+     * Constructs a {@link LocalizationProviderBridge} that wraps the given provider
+     * and registers itself as a {@link java.awt.event.WindowListener} to the specified
+     * {@link JFrame}.
+     *
+     * @param provider the provider to wrap
+     */
+    public FormLocalizationProvider(ILocalizationProvider provider, JFrame frame) {
+        super(provider);
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                connect();
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+                disconnect();
+            }
+        });
+    }
+}
