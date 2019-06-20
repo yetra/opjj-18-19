ec580200386f326180217d164915b5f6aaf2f8b7
Bruna <brdujmovic@gmail.com>
2019-06-20 21:34:28 +0200

    Expand the DAO interface

--- hw15-0036505665/src/main/java/hr/fer/zemris/java/tecaj_13/dao/DAO.java
+++ hw15-0036505665/src/main/java/hr/fer/zemris/java/tecaj_13/dao/DAO.java
@@ -1,17 +1,54 @@
 package hr.fer.zemris.java.tecaj_13.dao;
 
 import hr.fer.zemris.java.tecaj_13.model.BlogEntry;
+import hr.fer.zemris.java.tecaj_13.model.BlogUser;
+
+import java.util.List;
 
 public interface DAO {
 
 	/**
-	 * Dohvaća entry sa zadanim <code>id</code>-em. Ako takav entry ne postoji,
-	 * vraća <code>null</code>.
+	 * Returns a blog entry that has the specified {@code id}. If no such entry exists
+	 * in the database, this method returns {@code null}.
 	 * 
-	 * @param id ključ zapisa
-	 * @return entry ili <code>null</code> ako entry ne postoji
-	 * @throws DAOException ako dođe do pogreške pri dohvatu podataka
+	 * @param id the id of the blog entry
+	 * @return the blog entry or {@code null} if no such entry exists
+	 * @throws DAOException if there's an error with retrieving the data
 	 */
-	public BlogEntry getBlogEntry(Long id) throws DAOException;
-	
+	BlogEntry getBlogEntry(Long id) throws DAOException;
+
+	/**
+	 * Returns a list of all blog users.
+	 *
+	 * @return a list of all blog users
+	 * @throws DAOException if there's an error with retrieving the data
+	 */
+	List<BlogUser> getBlogUsers() throws DAOException;
+
+	/**
+	 * Returns a {@link BlogUser} of the specified nickname or {@code null} if no such
+	 * user exists.
+	 *
+	 * @param nick the nickname of the user
+	 * @return a {@link BlogUser} of the specified nickname or {@code null} if no such
+     *         user exists
+     * @throws DAOException if there's an error with retrieving the data
+	 */
+	BlogUser getBlogUser(String nick) throws DAOException;
+
+    /**
+     * Returns a list of blog entries created by the specified user.
+     *
+     * @param nick the nickname of the user
+     * @return a list of blog entries created by the specified user
+     * @throws DAOException if there's an error with retrieving the data
+     */
+	List<BlogEntry> getBlogEntriesFor(String nick) throws DAOException;
+
+    /**
+     * Adds a new {@link BlogUser} to the database.
+     *
+     * @param user the {@link BlogUser} to add
+     */
+	void addBlogUser(BlogUser user);
 }
